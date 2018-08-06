649b4e8585dc34aa94b6f2e1c78a0c848f0856f5
audreybernstein <audrey.bernstein@aol.com>
2018-08-06 19:10:37 +0000

    automatically backed up by learn

--- userinput.rb
+++ userinput.rb
@@ -1,5 +1,6 @@
 puts "What's your name?"
 name = gets.chomp
 
-puts "What's your favorite channel?"
-puts "My name is #{name} and you're watching Disney Channel"
+puts "What's your favorite channel?" 
+channel = gets.chomp
+puts "My name is #{name} and you're watching #{channel}"
